a32648beb35f9f7a2eeb986982b1a194ea5984ec
Steven Arcangeli <arcangeli07@gmail.com>
2010-12-08 05:52:51 -0500

    small fix to shell script

--- scripts/gen_conf.sh
+++ scripts/gen_conf.sh
@@ -4,5 +4,5 @@ source /etc/bs-tester.conf
 MAP=$1
 SEED=$2
 cd $INSTALL_DIR/repo
-cat bc.conf | sed -i -e 's/bc.game.maps=.*/bc.game.maps='"$MAP"'/' -e 's/bc.game.team-a=.*/bc.game.team-a=old_team_a/' -e 's/bc.game.team-b=.*/bc.game.team-b=old_team_b/' -e 's/bc.server.save-file=.*/bc.server.save-file='"$MAP"'.rms/'
-cat maps/$MAP.xml | sed -i -e 's/seed="[^ ]*"/seed="'"$SEED"'"/' 
+sed -i -e 's/bc.game.maps=.*/bc.game.maps='"$MAP"'/' -e 's/bc.game.team-a=.*/bc.game.team-a=old_team_a/' -e 's/bc.game.team-b=.*/bc.game.team-b=old_team_b/' -e 's/bc.server.save-file=.*/bc.server.save-file='"$MAP"'.rms/' bc.conf
+sed -i -e 's/seed="[^ ]*"/seed="'"$SEED"'"/' maps/$MAP.xml
