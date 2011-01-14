130e146405d5dbf963d952e9f96f353aee6e5402
Steven Arcangeli <arcangeli07@gmail.com>
2011-01-14 18:23:13 -0500

    Fixed installation bug

--- scripts/install_daemon.sh
+++ scripts/install_daemon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SERVER=$1
 DIR=${2//\//\\\/}
-cat etc/init.d/bs-tester | sed -e 's/SERVER => 1/SERVER => '"$SERVER"'/' -e 's/BS_TESTER_DIR.*/BS_TESTER_DIR => "'"$DIR"'"/' > /etc/init.d/bs-tester
+cat etc/init.d/bs-tester | sed -e 's/SERVER => 1/SERVER => '"$SERVER"'/' -e 's/BS_TESTER_DIR => .*/BS_TESTER_DIR => "'"$DIR"'";/' > /etc/init.d/bs-tester
+chmod +x /etc/init.d/bs-tester
 update-rc.d bs-tester defaults
