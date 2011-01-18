ff365cfead1f9766f26bd97fd564a78b04e9935c
Steven Arcangeli <arcangeli07@gmail.com>
2011-01-18 12:17:56 -0500

    Improved setup script.  Tested mercurial support (seems fine)

--- setup.sh
+++ setup.sh
@@ -20,6 +20,16 @@ if [[ `which java` == "" ]]; then
   exit 1
 fi
 
+if [[ `which tar` == "" ]]; then
+  echo "tar not found"
+  exit 1
+fi
+
+if [[ `which gzip` == "" ]]; then
+  echo "gzip not found"
+  exit 1
+fi
+
 if [ -e /etc/bs-tester.conf ]; then
   if [ "$1" != "-f" ]; then
     echo "Setup completed.  To reconfigure type ./setup.sh -f"
@@ -31,6 +41,8 @@ if [ ! -e ./run.sh ]; then
   echo "Must run setup.sh locally (./setup.sh)"
   exit 1
 fi
+
+MASTER=1;
 
 INSTALL_DIR=`pwd`
 
@@ -93,7 +105,7 @@ setup_worker() {
   
 }
 
-setup_server() {
+setup_master() {
   VERSION_OPTIONS=""
   for DIR in `find scripts/* -type d | sed -e 's/scripts\///'`; do
     VERSION_OPTIONS=$VERSION_OPTIONS"/"$DIR
@@ -196,29 +208,31 @@ setup_server() {
   # Generate bs-worker.tar.gz
   DIR=`pwd | sed -e 's/.*\///'`
   cd ..
+  # adjust setup script to run for workers
+  sed -i -e 's/MASTER=1/MASTER=0/' $DIR/setup.sh
   tar -cf $DIR/bs-worker.tar $DIR/bs-tester.jar $DIR/etc $DIR/keystore $DIR/README $DIR/COPYING $DIR/run.sh $DIR/setup.sh $DIR/scripts $DIR/uninstall.sh
   gzip $DIR/bs-worker.tar
+  # reset setup script
+  sed -i -e 's/MASTER=0/MASTER=1/' $DIR/setup.sh
+
+  echo "Setup completed!  All files for setting up a worker are in bs-worker.tar.gz"
 }
 
-SERVER=0
-if [ -e bs-worker.tar.gz ]; then
-  SERVER=1
-elif [ ! -e keystore ]; then
-  SERVER=1
-fi
-
-# if the -f option is passed, remove the current repo
+# if the -f option is passed, remove the current repo and config file
 if [ "$1" == "-f" ]; then
-  rm -rf repo > /dev/null 2> /dev/null
-  if [ "$SERVER" == "1" ]; then
-    rm keystore > /dev/null 2> /dev/null
-    rm bs-worker.tar > /dev/null 2> /dev/null
-    rm bs-worker.tar.gz > /dev/null 2> /dev/null
+  echo "Cleaning out old files..."
+  rm -rf repo
+  rm -f /etc/bs-tester.conf
+  rm -rf hsqldb*
+  if [ "$MASTER" == "1" ]; then
+    rm keystore
+    rm bs-worker.tar
+    rm bs-worker.tar.gz
   fi
 fi
 
-if [ "$SERVER" == "1" ]; then
-  setup_server
+if [ "$MASTER" == "1" ]; then
+  setup_master
 else
   setup_worker
 fi
