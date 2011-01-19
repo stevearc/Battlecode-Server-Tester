75f1d4c2514f1811e7c65e4468eff92cbeef3da5
Steven Arcangeli <arcangeli07@gmail.com>
2011-01-19 18:33:54 -0500

    Force users to run run.sh locally

--- run.sh
+++ run.sh
@@ -5,4 +5,10 @@ if [ $ID != 0 ]; then
   exit 1
 fi
 
+if [ ! -e bs-tester.jar ]; then
+  echo "This script must be run locally."
+  echo "Try changing to the directory that run.sh is in."
+  exit 1
+fi
+
 java -jar bs-tester.jar $*
