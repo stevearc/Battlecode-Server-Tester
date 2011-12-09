ca493afa58f104990f5abb5e3393787b145d2eee
Steven Arcangeli <steven.arcangeli@redfin.com>
2011-12-08 19:15:13 -0800

    Migrated packaging/build framework over to Ant

--- pack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -e bs-tester.tar.gz ]; then
-  rm bs-tester.tar.gz
-fi
-
-cp -r bs-tester.jar README COPYING scripts etc run.sh setup.sh uninstall.sh js css images bs-tester 
-tar -cvvf bs-tester.tar bs-tester
-gzip bs-tester.tar
-rm -rf bs-tester/*
