fca96cb48267c30e50a664c68189369b1a67dff3
Steven Arcangeli <arcangeli07@gmail.com>
2011-12-24 11:37:37 -0800

    Fixed layout of embedded stats iframe

--- static/js/matches.js
+++ static/js/matches.js
@@ -21,25 +21,24 @@ $(function() {
 });
 
 function rowClick(matchId) {
-    $('#graphs').remove();
-    $('#closeButton').remove();
-    $('#navButton').remove();
+    $('#container').remove();
     var div = $("<div/>").attr('class','overlay').attr('id', 'overlay').click(function() {
         $('#overlay').remove();
-        $('#graphs').remove();
-        $('#closeButton').remove();
-        $('#navButton').remove();
+        $('#container').remove();
     }).appendTo("body");
-    $("<iframe id='graphs' />")
-    .attr("src", 'analysis_content.html?id=' + matchId)
-    .attr("style", "width:1000px; height:600px; margin-left: -10px; position:fixed; top:100px; z-index:1010")
-    .appendTo("body");
+    var container = $("<div/>").attr('id', 'container')
+        .attr('style', 'position: absolute; top:100px; text-align:center; width:1000px; margin-left:-10px; z-index:1010; background: #FFF;')
+        .appendTo("body");
+    $("<iframe />")
+        .attr("src", 'analysis_content.html?id=' + matchId)
+        .attr("style", "width:1000px; height:570px; border:0;")
+        .appendTo(container);
 
-    $("<button id='navButton'>View in full page</button>")
-    .attr("style", "position:fixed; top:660px; left: 900px; z-index:1020")
-    .button()
-    .click(function() {
-        document.location='analysis.html?id=' + matchId;
-    })
-    .appendTo("body");
+    $("<button>View in full page</button>")
+        .attr("style", "")
+        .button()
+        .click(function() {
+            document.location='analysis.html?id=' + matchId;
+        })
+        .appendTo(container);
 }
